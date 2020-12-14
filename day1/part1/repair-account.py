7972bf783018ffc6f484d883dd1bac19a8daf652
Kartik Sharma <kartik4949@gmail.com>
2020-12-14 13:37:25 +0530

    part1 challenge 2 completed

--- day1/part1/repair-account.py
+++ day1/part1/repair-account.py
@@ -3,5 +3,6 @@ with open('account.txt', 'r') as r:
     account = list(map(lambda x: int(x.strip()), account))
 
 import itertools
-price = [p1*p2 for p1, p2 in itertools.product(account, account) if p1+p2 == 2020][0]
-print(price)
+price1 = [p1*p2 for p1, p2 in itertools.product(account, account) if p1+p2 == 2020][0]
+price2 = [p1*p2*p3 for p1, p2, p3 in itertools.product(account, account, account) if p1+p2+p3 == 2020][0]
+print(price1,price2)
