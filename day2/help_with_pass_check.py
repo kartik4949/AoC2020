8ebf127e27a343170df82bac2f9fa977cca3e6c7
Kartik Sharma <kartik4949@gmail.com>
2020-12-14 15:13:03 +0530

    refactor

--- day2/help_with_pass_check.py
+++ day2/help_with_pass_check.py
@@ -1,26 +1,20 @@
 with open('pass.txt', 'r') as r:
     passwords = [ l.strip().split(' ') for l in r.readlines()]
-valid = 0
-# -----------------------part2--------------------------------------- 
+valid1 = 0
+valid2 = 0
+# -----------------------part 1-2--------------------------------------- 
 for password in passwords:
     _pass_range = tuple(map(int, password[0].split('-')))
     token = password[1][0]
     password = password[-1]
+
     token_count = password.count(token)
     if token_count >= _pass_range[0] and token_count <= _pass_range[1]:
-        valid += 1
-print(valid)
-valid = 0
+        valid1 += 1
 
-
-
-# -----------------------part2--------------------------------------- 
-for password in passwords:
-    _pass_range = tuple(map(int, password[0].split('-')))
-    token = password[1][0]
-    password = password[-1]
     p1 = password[_pass_range[0]-1] == token
     p2 = password[_pass_range[1]-1] == token
     if p1!=p2:
-        valid += 1
-print(valid)
+        valid2 += 1
+print(valid1)
+print(valid2)
