8a09903f2772b56266cb1e4506f32d9ece6a33de
Kartik Sharma <kartik4949@gmail.com>
2020-12-20 10:55:34 +0530

    day 11 clean up

--- day11/map.py
+++ day11/map.py
@@ -74,25 +74,58 @@ def simulate(grid_layout):
 with open("input.txt") as r:
     grid_layout = r.readlines()
 
+
+def visualize(grid_layout):
+    gw = []
+    for gr in grid_layout:
+        gw.append("".join(gr))
+        gw += ["\n"]
+    gw = "".join(gw)
+    print(gw)
+# parse
+width = len(grid_layout[0].strip())
+height = len(grid_layout)
+
+def parse(grid_layout):
+    new_grid = []
+    for i in range(height):
+        new_grid.append([g for g in grid_layout[i] if g != "\n"])
+    return new_grid
+grid_layout= parse(grid_layout)
 import copy
 original_grid = copy.deepcopy(grid_layout)
-grid_layout= parse(grid_layout)
 
+def get_adj_count(adjacents):
+    adj_occ_count = 0
+    for a in adjacents:
+        if a == "#":
+            adj_occ_count+=1
+    return adj_occ_count, len(adjacents) -adj_occ_count
 def cell_decision2(grid, seat_id, no_count):
     global original_grid
     i,j = seat_id
     adjacents = []
     for coor in [[1,1], [-1,-1], [1,-1], [-1,1], [0,1], [1,0], [0,-1], [-1,0]]:
         i_c,j_c = coor
+        move_y, move_x = i_c, j_c
+
         i_c += i
         j_c += j
-        if (i_c>=0 and i_c<height) and (j_c>=0 and j_c<width):
-            adjacents.append(original_grid[i_c][j_c])
+        while True:
+            if (i_c>=0 and i_c<height) and (j_c>=0 and j_c<width):
+                if original_grid[i_c][j_c] != '.':
+                    adjacents.append(original_grid[i_c][j_c])
+                    break
+            else:
+                break
+
+            i_c+=move_y
+            j_c+=move_x
 
     adj_occ, adj_not_occ = get_adj_count(adjacents)
     if original_grid[i][j] == "L" and adj_not_occ == len(adjacents):
         grid[i][j] = "#"
-    elif original_grid[i][j]  == "#" and adj_occ >= 4:
+    elif original_grid[i][j]  == "#" and adj_occ >= 5:
         grid[i][j]= "L"
     else:
         no_count += 1
@@ -100,10 +133,17 @@ def cell_decision2(grid, seat_id, no_count):
     return grid, no_count
 
 
+def change_grid2(grid_layout):
+    no_count = 0
+    for i in range(height):
+        for j in range(width):
+            grid_layout, no_count = cell_decision2(grid_layout, (i,j), no_count)
+    return grid_layout ,no_count == (width*height)
 
-def simulate(grid_layout):
+def simulate2(grid_layout):
+    global original_grid
     while True:
-        grid_layout, stable = change_grid(grid_layout)
+        grid_layout, stable = change_grid2(grid_layout)
         original_grid = copy.deepcopy(grid_layout)
         if stable:
             break
@@ -111,3 +151,4 @@ def simulate(grid_layout):
     for grid_row in grid_layout:
         occ_count += grid_row.count("#")
     print(occ_count)
+simulate2(grid_layout)
