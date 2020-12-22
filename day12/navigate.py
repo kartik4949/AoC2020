d7c9f0fd2a090b9073ebdbc496e54fc82684e631
Kartik Sharma <kartik4949@gmail.com>
2020-12-22 15:44:45 +0530

    day 12 part2

--- day12/navigate.py
+++ day12/navigate.py
@@ -52,20 +52,26 @@ DIRECTION = ["E", "S", "W", "N"]
 MOVE = [0,0,0,0]
 WAYPOINT = [10,0,0,1]
 
-def rotate90(x,direction, i,c=True):
+def rotate90(x,direction, i,c):
+    global WAYPOINT
     new_x = (direction + i)%4 if c else direction - i
     new_x = new_x if new_x >=0 else 4 + new_x
-    WAYPOINT[new_x] = x
     WAYPOINT[direction]=0
+    return new_x, x
+    #WAYPOINT[new_x] = x
 
-def waypoint_rot(rotidx):
-    x = max(WAYPOINT[0],WAYPOINT[2])
-    ix = WAYPOINT.index(x)
-    y = max(WAYPOINT[1],WAYPOINT[3])
-    iy = WAYPOINT.index(y)
-    c = True if rotidx>0 else False
-    rotate90(x, ix,rotidx,c)
-    rotate90(y,iy,rotidx,c)
+def waypoint_rot(rotidx,c):
+    global WAYPOINT
+    iyd = np.argmax(np.asarray(WAYPOINT)[[1,3]])
+    iy = 1 if iyd == 0 else 3
+    y = WAYPOINT[iy]
+    ixd = np.argmax(np.asarray(WAYPOINT)[[0,2]])
+    ix = 0 if ixd == 0 else 2
+    x = WAYPOINT[ix]
+    new_x_idx,xv = rotate90(x, ix,rotidx,c)
+    new_y_idx, yv =rotate90(y,iy,rotidx,c)
+    WAYPOINT[new_x_idx] = xv
+    WAYPOINT[new_y_idx] = yv
 
 
 class Ship:
@@ -78,7 +84,7 @@ class Ship:
 def move_waypoint(move, d):
     global WAYPOINT
     opp_move =  WAYPOINT[(d +2)%4]
-    if ship.direction == d and opp_move ==0:
+    if WAYPOINT[d] > 0 and opp_move ==0:
         WAYPOINT[d] += move
     else:
         if move > opp_move:
@@ -100,27 +106,30 @@ def move_ship(ship, move, d):
 
         elif move <= opp_move:
             MOVE[(d +2)%4] = int(opp_move - move)
+import numpy as np
 def action(ship, d, move):
     global DIRECTION
+    global WAYPOINT
     if d == "F":
-        movey = max(WAYPOINT[1],WAYPOINT[3])
-        ixd = WAYPOINT.index(movey)
-        movex = max(WAYPOINT[0],WAYPOINT[2])
-        iyd = WAYPOINT.index(movex)
-        move_ship(ship, movey, iyd)
-        move_ship(ship, movex, ixd)
+        iyd = np.argmax(np.asarray(WAYPOINT)[[1,3]])
+        iyd = 1 if iyd == 0 else 3
+        movey = WAYPOINT[iyd]
+        ixd = np.argmax(np.asarray(WAYPOINT)[[0,2]])
+        ixd = 0 if ixd == 0 else 2
+        movex = WAYPOINT[ixd]
+        move_ship(ship, movey*move, iyd)
+        move_ship(ship, movex*move, ixd)
     elif d == "R" or d=="L":
         i = move // 90
-        waypoint_rot(i)
+        waypoint_rot(i,True if d =="R" else False)
     else:
-        move_ship(waypoint, move,DIRECTION.index(d))
+        move_waypoint( move,DIRECTION.index(d))
 
 def decode_direction(d):
     return d[0], int(d[1:])
 
 ship = Ship()
-waypoint = Waypoint()
 for d in directions:
    direction, move = decode_direction(d)
-   action(ship, waypoint, direction, move)
+   action(ship,direction, move)
 print(sum(MOVE))
